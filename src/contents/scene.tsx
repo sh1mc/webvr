3506519102f219c154949e20ba06f95ed6e44b33
sh1mc <sh1@coins.tsukuba.ac.jp>
2021-08-31 09:41:24 +0900

    add video skybox

--- src/contents/scene.tsx
+++ src/contents/scene.tsx
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
-import { Text, Box } from "@react-three/drei"
-import { useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
+import React, {useEffect, useRef, useState} from "react"
+import {Text, Box, useAspect, Sky, useFBX} from "@react-three/drei"
+import {useFrame, useThree} from "@react-three/fiber"
+import {Mesh, VideoTexture} from "three"
+import * as THREE from "three"
 
 const Scene: React.FC = () => {
 	return (
@@ -11,7 +12,7 @@ const Scene: React.FC = () => {
 
 const Thing: React.FC = () => {
 	const ref = useRef({} as Mesh)
-	useFrame(({ clock }) => {
+	useFrame(({clock}) => {
 		const T = 2.0
 		const sec = clock.getElapsedTime() % T
 		const fixedSec = sec / T
@@ -29,7 +30,7 @@ const Thing: React.FC = () => {
 
 const RotatingText: React.FC = () => {
 	const ref = useRef({} as Mesh)
-	useFrame(({ clock }) => {
+	useFrame(({clock}) => {
 		const T = 2.0
 		const sec = clock.getElapsedTime() % T
 		const fixedSec = sec / T
@@ -55,4 +56,28 @@ const RotatingText: React.FC = () => {
 	)
 }
 
-export { Scene, RotatingText }
+const VideoObject: React.FC = () => {
+	const [video] = useState(() => {
+		const vid = document.createElement("video")
+		vid.src = "/a.mp4"
+		vid.crossOrigin = "Anonymous"
+		vid.loop = true
+		return vid
+	})
+	const play = (() => {
+		video.play();
+	})
+	useEffect(() => {
+		document.addEventListener("mousedown", play, {once: true})
+	}, [])
+	return (
+		<mesh scale={[10, 10, 10]} position={[0, 0, 0]}>
+			<boxBufferGeometry />
+			<meshBasicMaterial side={THREE.DoubleSide}>
+				<videoTexture attach="map" args={[video]} />
+			</meshBasicMaterial>
+		</mesh>
+	)
+}
+
+export {Scene, RotatingText, VideoObject}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { VRCanvas, DefaultXRControllers, Hands } from '@react-three/xr'
+import {VRCanvas, DefaultXRControllers, Hands} from '@react-three/xr'
 import styled from 'styled-components'
-import { Scene, RotatingText } from '../contents/scene'
-import { Stars, Text } from '@react-three/drei'
+import {Scene, RotatingText, VideoObject} from '../contents/scene'
+import {Sky, Stars, Text} from '@react-three/drei'
 
 const Home: React.FC = () => {
 	return (
 		<Layout>
 			<VRCanvasWrapper>
 				<VRCanvas>
-					<ambientLight intensity={0.5} />
-					<pointLight position={[5, 5, 5]} />
-					<Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade />
-					<Scene />
-					<RotatingText />
+					<VideoObject />
 					<Hands />
 					<DefaultXRControllers />
 				</VRCanvas>
